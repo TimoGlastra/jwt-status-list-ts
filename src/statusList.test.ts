c57f4e81e2e8e9411c08e39bbf6cff6a65ecfb21
Timo Glastra <timo@animo.id>
2023-10-13 13:36:20 +0200

    test: rename bits to list
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/statusList.test.ts
+++ src/statusList.test.ts
@@ -3,30 +3,30 @@ import { decodeStatusList, encodeStatusList } from "./statusList";
 
 // https://datatracker.ietf.org/doc/html/draft-looker-oauth-jwt-cwt-status-list-01#name-example-status-list-with-2-
 const list1Bit = {
-  bits: [1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
+  list: [1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
   encoded: "H4sIAMo_jGQC_9u5GABc9QE7AgAAAA",
 };
 
 // https://datatracker.ietf.org/doc/html/draft-looker-oauth-jwt-cwt-status-list-01#name-example-status-list-with-2-
 const list2Bits = {
-  bits: [1, 2, 0, 3, 0, 1, 0, 1, 1, 2, 3, 3],
+  list: [1, 2, 0, 3, 0, 1, 0, 1, 1, 2, 3, 3],
   encoded: "H4sIAMo_jGQC_zvp8hMAZLRLMQMAAAA",
 };
 
 describe("jwtStatusList", () => {
   test("should encode jwt status list with bit size 1", () => {
-    expect(encodeStatusList(list1Bit.bits, 1)).toBe(list1Bit.encoded);
+    expect(encodeStatusList(list1Bit.list, 1)).toBe(list1Bit.encoded);
   });
 
   test("should encode jwt status list with bit size 2", () => {
-    expect(encodeStatusList(list2Bits.bits, 2)).toBe(list2Bits.encoded);
+    expect(encodeStatusList(list2Bits.list, 2)).toBe(list2Bits.encoded);
   });
 
   test("should decode jwt status list with bit size 1", () => {
-    expect(decodeStatusList(list1Bit.encoded, 1)).toEqual(list1Bit.bits);
+    expect(decodeStatusList(list1Bit.encoded, 1)).toEqual(list1Bit.list);
   });
 
   test("should decode jwt status list with bit size 2", () => {
-    expect(decodeStatusList(list2Bits.encoded, 2)).toEqual(list2Bits.bits);
+    expect(decodeStatusList(list2Bits.encoded, 2)).toEqual(list2Bits.list);
   });
 });
